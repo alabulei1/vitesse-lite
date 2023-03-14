cb34a670fa8e6c1d6d35b1c060e8e8d23038ddc5
Ji Chen <jaykchen@gmail.com>
2023-03-14 13:19:09 -0400

    Update component.test.ts

--- test/component.test.ts
+++ test/component.test.ts
@@ -7,16 +7,3 @@ describe('TheCounter.vue', () => {
     const wrapper = mount(TheCounter, { props: { initial: 10 } })
     expect(wrapper.text()).toContain('10')
     expect(wrapper.html()).toMatchSnapshot()
-  })
-
-  it('should be interactive', async () => {
-    const wrapper = mount(TheCounter, { props: { initial: 0 } })
-    expect(wrapper.text()).toContain('0')
-
-    expect(wrapper.find('.inc').exists()).toBe(true)
-
-    await wrapper.get('button').trigger('click')
-
-    expect(wrapper.text()).toContain('1')
-  })
-})
