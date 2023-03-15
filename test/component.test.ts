d60c1086f4cac47b55ac1a6da5d80ff9066b1582
Ji Chen <jaykchen@gmail.com>
2023-03-15 16:53:24 -0400

    Update component.test.ts (#36)

--- test/component.test.ts
+++ test/component.test.ts
@@ -2,5 +2,4 @@ import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import TheCounter from '../src/components/TheCounter.vue'
 
-describe('TheCounter.vue', () => {
-  it('should 
+describ
