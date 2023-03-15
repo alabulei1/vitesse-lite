8fe6c51b530107ce32ceae55f60f359c3f966693
Ji Chen <jaykchen@gmail.com>
2023-03-15 14:49:58 -0400

    Update component.test.ts (#35)

--- test/component.test.ts
+++ test/component.test.ts
@@ -3,7 +3,4 @@ import { describe, expect, it } from 'vitest'
 import TheCounter from '../src/components/TheCounter.vue'
 
 describe('TheCounter.vue', () => {
-  it('should render', () => {
-    const wrapper = mount(TheCounter, { props: { initial: 10 } })
-    expect(wrapper.text()).toContain('10')
-    expect(wrapper.html()).toMatchSnapshot()
+  it('should 
